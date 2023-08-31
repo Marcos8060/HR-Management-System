d9f6302da8ac843fbe142392c31a6cd8d63f41ba
MarcosDMT <marcosgav80@gmail.com>
2023-08-31 13:09:47 +0300

    added a vertical scrollbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useRef, useEffect } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { Drawer } from "./Drawer";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 type props = {
   collapsed: boolean;
@@ -10,6 +13,18 @@ const Header = ({ collapsed }: props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [navBackground, setNavBackground] = useState(false);
   const [show, setShow] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setAnchorEl(event.currentTarget as HTMLElement);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const navRef = useRef<boolean>();
   navRef.current = navBackground;
@@ -31,7 +46,7 @@ const Header = ({ collapsed }: props) => {
     <>
       <section className="flex justify-between sm:px-8 px-2 py-4 sticky top-0 z-50 shadow-xl bg-primary text-white">
         <div className="">
-          <h1 className="text-xl">Main Dashboard</h1>
+          <h1 className="text-xl">MarcosDev</h1>
         </div>
 
         <div className="flex items-center gap-4">
@@ -42,19 +57,28 @@ const Header = ({ collapsed }: props) => {
             />
             <Drawer {...{ isOpen, setIsOpen, collapsed }} />
           </div>
-          <img
-            onClick={() => setShow((prev) => !prev)}
-            className="w-10 h-10 rounded-full cursor-pointer"
-            src="/images/me.png"
-            alt=""
-          />
+          <Button onClick={handleClick}>
+            <img
+              className="w-10 h-10 rounded-full cursor-pointer"
+              src="/images/me.png"
+              alt=""
+            />
+          </Button>
+
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
         </div>
-        {show && (
-          <div className="bg-white px-2 space-y-2 py-4 shadow-2xl absolute top-14 sm:right-10 right-4 rounded w-24 h-28 text-black">
-            <p className="">Profile</p>
-            <p className="">Logout</p>
-          </div>
-        )}
       </section>
     </>
   );
