0cd63a7a1ff0a0d92aa2f39876e26749b57e66f5
MarcosDMT <marcosgav80@gmail.com>
2023-09-01 07:48:32 +0300

    added projects table

--- src/pages/dashboard/admin/tables/index.tsx
+++ src/pages/dashboard/admin/tables/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import Clients from './clients'
+import React from "react";
+import Clients from "./clients";
+import { Grid } from "@mui/material";
+import Projects from "./projects";
 
 const Tables = () => {
   return (
-    <section className='my-8'>
-    <Clients />
-    </section>
-  )
-}
+    <>
+      {/* <section className="my-4 space-y-8">
+        <Clients />
+        <Projects />
+      </section> */}
+      <Grid container spacing={2} sx={{ my: 2 }}>
+        <Grid item md={6} xs={12}>
+          <Clients />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Projects />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
-export default Tables
+export default Tables;
--- src/pages/dashboard/admin/tables/projects.tsx
+++ src/pages/dashboard/admin/tables/projects.tsx
@@ -6,27 +6,23 @@ const DataGrid = dynamic(() => import("devextreme-react/data-grid"), {
   ssr: false,
 });
 
-const Clients = () => {
+const Projects = () => {
   const users = [
     {
-      name: "Marcos Ochieng",
-      email: "marcos@gmail.com",
-      status: "Active",
+      project_name: "Hospital Management System",
+      progress: "Inprogres",
     },
     {
-      name: "Derrick Ochieng",
-      email: "marcos@gmail.com",
-      status: "Active",
+      project_name: "Admin Portal",
+      progress: "completed",
     },
     {
-      name: "Jane Ochieng",
-      email: "marcos@gmail.com",
-      status: "Active",
+      project_name: "Admin Portal",
+      progress: "On hold",
     },
     {
-      name: "Ann Ochieng",
-      email: "marcos@gmail.com",
-      status: "Active",
+      project_name: "Admin Portal",
+      progress: "Under Review",
     },
   ];
   return (
@@ -43,23 +39,17 @@ const Clients = () => {
         height={"60vh"}
         className="shadow-xl"
       >
-        <Column dataField="name" caption="Name" width={200} />
-        <Column dataField="email" caption="Email" width={200} />
+        <Column dataField="project_name" caption="Project Name" width={200} />
+        <Column dataField="progress" caption="Progress" width={200} />
         <Column
           dataField="status"
-          caption="Status"
-          width={200}
-          allowFiltering={false}
-        />
-        <Column
-        //   dataField="status"
           caption="Action"
           width={140}
           allowFiltering={false}
         />
         <Toolbar>
           <Item location="before">
-            <p className="text-xl">Clients</p>
+            <p className="text-xl">Recent Projects</p>
           </Item>
         </Toolbar>
       </DataGrid>
@@ -67,4 +57,4 @@ const Clients = () => {
   );
 };
 
-export default Clients;
+export default Projects;
