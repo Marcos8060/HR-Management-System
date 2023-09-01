2066521de11aebdad450d638fd7c45f0037378ac
MarcosDMT <marcosgav80@gmail.com>
2023-09-01 15:08:44 +0300

    added mui themeprovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
-import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import 'devextreme/dist/css/dx.material.blue.light.css';
-import '../styles/devextreme.css';
-
+import "devextreme/dist/css/dx.material.blue.light.css";
+import "../styles/devextreme.css";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = (Component as any).getLayout ?? ((page:any) => page)
+  const getLayout = (Component as any).getLayout ?? ((page: any) => page);
 
   return (
-      getLayout(<Component {...pageProps} />)
+    <ThemeProvider theme={theme}>
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
   );
 }
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -5,25 +5,24 @@ import * as yup from "yup";
 import { useFormik } from "formik";
 
 const Login = () => {
+  const validationSchema = yup.object({
+    username: yup.string().required("Username is required"),
+    password: yup.string().required("Password is required"),
+  });
 
-    const validationSchema = yup.object({
-        username: yup.string().required("Username is required"),
-        password: yup.string().required("Password is required"),
-      });
-    
-      const formik = useFormik({
-        initialValues: {
-          username: "",
-          password: "",
-        },
-        validationSchema: validationSchema,
-        onSubmit: async (values, helpers) => {
-          try {
-            // await loginUser(values.username,values.password);
-            helpers.resetForm();
-          } catch (error) {}
-        },
-      });
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values, helpers) => {
+      try {
+        // await loginUser(values.username,values.password);
+        helpers.resetForm();
+      } catch (error) {}
+    },
+  });
   return (
     <section className="bg-surface h-screen flex items-center justify-center">
       <section className="bg-white shadow-xl w-4/12 mx-auto rounded p-4 space-y-8">
@@ -31,9 +30,23 @@ const Login = () => {
           <h1 className="text-center text-xl">Login</h1>
           <p className="text-center">Access to our dashboard</p>
         </div>
-        <form className="flex flex-col space-y-4">
-          <TextField label="username" />
-          <TextField label="password" />
+        <form className="flex flex-col space-y-4" onSubmit={formik.handleSubmit}>
+          <TextField
+            label="username"
+            name="username"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.username && Boolean(formik.errors.username)}
+            helperText={formik.touched.username && formik.errors.username}
+          />
+          <TextField
+            label="password"
+            name="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
           <button className="bg-primary rounded px-4 py-2 text-white w-full">
             Login
           </button>
