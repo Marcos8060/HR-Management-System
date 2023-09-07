fd2f9d254a34916c44424cf9aa36abf990eac71e
MarcosDMT <marcosgav80@gmail.com>
2023-09-07 18:37:26 +0300

    added list of permissions

---
+++ src/assets/utils/permissions.tsx
@@ -0,0 +1,23 @@
+export const PERMISSIONS = {
+    DASHBOARD: {
+      ACCESS: 'Permissions.Dashboard.Access',
+      VIEW: 'Permissions.Dashboard.View',
+    },
+    EMPLOYEES: {
+      ACCESS: 'Permissions.Employees.Access',
+      VIEW: 'Permissions.Employees.View',
+      CREATE: 'Permissions.Employees.Create',
+      UPDATE: 'Permissions.Employees.Update',
+      DELETE: 'Permissions.Employees.Delete',
+      SEARCH: 'Permissions.Employees.Search',
+    },
+    PROJECTS: {
+      ACCESS: 'Permissions.Projects.Access',
+      VIEW: 'Permissions.Projects.View',
+      CREATE: 'Permissions.Projects.Create',
+      UPDATE: 'Permissions.Projects.Update',
+      DELETE: 'Permissions.Projects.Delete',
+      SEARCH: 'Permissions.Projects.Search',
+    },
+  };
+  
