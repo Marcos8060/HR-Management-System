1719e8d9db4966689023b551deb13c25f13dda0c
MarcosDMT <marcosgav80@gmail.com>
2023-09-11 15:03:47 +0300

    rendered routes based on user permissions

--- src/assets/utils/hoc/protected-route.tsx
+++ src/assets/utils/hoc/protected-route.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React,{ReactNode} from 'react'
+import { useSelector } from 'react-redux'
 import { menus } from '../menu'
-import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector'
+import { useRouter } from 'next/router'
+import NotAuthorized from '@/pages/dashboard/not-authorised'
 
-const ProtectedRoute = () => {
+type ProtectedRouteProps = {
+    href: string;
+    permission: string,
+    children: ReactNode
+  }
+
+  type Permission = {
+    id: number;
+    permission: string;
+    role_name: string;
+  };
+
+const ProtectedRoute = ({ permission,children }: ProtectedRouteProps) => {
     const { userPermissions } = useSelector(( { auth }) => auth)
+
+
+    // check if current user has permission to access the route in question
+    const isAuthorized = userPermissions && userPermissions.find((perm: Permission) => perm.permission === permission)
+
+
+    if (!isAuthorized) {
+      return <NotAuthorized />
+    }
+
   return (
-    <div>ProtectedRoute</div>
+    <>{ children }</>
   )
 }
 
--- src/pages/dashboard/admin/index.tsx
+++ src/pages/dashboard/admin/index.tsx
@@ -6,35 +6,38 @@ import SalesChart from "./charts/sales-chart";
 import Statistics from "./statistics";
 import Tables from "./tables";
 import Layout from "@/components/Layout";
+import ProtectedRoute from "@/assets/utils/hoc/protected-route";
 
 const Admin = () => {
   return (
-    <Container maxWidth="xl" className="py-4">
-      <h1 className="text-xl mb-4">Welcome Admin</h1>
-      <Grid container spacing={2}>
-        {dashboardData.map((data, index) => (
-          <Grid item md={3} xs={12} key={index}>
-            <section className="bg-white shadow-xl rounded flex items-center justify-between py-2 px-4 h-28">
-              <div>{data.icon}</div>
-              <div className="text-center text-xl">
-                <p className="font-semibold">{data.number}</p>
-                <p>{data.label}</p>
-              </div>
-            </section>
+    <ProtectedRoute href="/dashboard/admin" permission="ACCESS_ADMIN_DASHBOARD">
+      <Container maxWidth="xl" className="py-4">
+        <h1 className="text-xl mb-4">Welcome Admin</h1>
+        <Grid container spacing={2}>
+          {dashboardData.map((data, index) => (
+            <Grid item md={3} xs={12} key={index}>
+              <section className="bg-white shadow-xl rounded flex items-center justify-between py-2 px-4 h-28">
+                <div>{data.icon}</div>
+                <div className="text-center text-xl">
+                  <p className="font-semibold">{data.number}</p>
+                  <p>{data.label}</p>
+                </div>
+              </section>
+            </Grid>
+          ))}
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item md={6} xs={12}>
+            <RevenueChart />
           </Grid>
-        ))}
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item md={6} xs={12}>
-          <RevenueChart />
+          <Grid item md={6} xs={12}>
+            <SalesChart />
+          </Grid>
         </Grid>
-        <Grid item md={6} xs={12}>
-          <SalesChart />
-        </Grid>
-      </Grid>
-      <Statistics />
-      <Tables />
-    </Container>
+        <Statistics />
+        <Tables />
+      </Container>
+    </ProtectedRoute>
   );
 };
 
