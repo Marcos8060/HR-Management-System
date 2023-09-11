b19970514ab885a8b087bacc427691f0466fdc2f
MarcosDMT <marcosgav80@gmail.com>
2023-09-11 08:51:45 +0300

    maded the admin dashboard index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import Login from "../AuthContext/login";
 import Admin from "./dashboard/admin";
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
     <>
-      <Login />
+      <Admin />
     </>
   );
 }
+
+Home.getLayout = (page: React.ReactNode) =>(
+  <Layout>{page}</Layout>
+)
