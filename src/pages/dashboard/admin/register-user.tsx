1af5ad7d5d4d923fbb911ae5c0fef58572fd43af
MarcosDMT <marcosgav80@gmail.com>
2023-09-20 07:09:35 +0300

    modified the registration page

--- src/pages/dashboard/admin/register-user.tsx
+++ src/pages/dashboard/admin/register-user.tsx
@@ -4,7 +4,7 @@ import * as Yup from "yup";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { FormikValues, FormikHelpers } from "formik";
 import { BiArrowBack } from "react-icons/bi";
-import Link  from 'next/link'
+import Link from "next/link";
 
 const RegisterUser = () => {
   const initialValues = {
@@ -57,71 +57,73 @@ const RegisterUser = () => {
   return (
     <ProtectedRoute permission="CAN_REGISTER_USERS">
       <section className="flex items-center justify-center h-screen">
-        <div className="bg-white rounded w-9/12 mx-auto p-4 h-[80vh]">
-          <Link href="/dashboard/admin" className="flex items-center gap-2">
-            <BiArrowBack className="text-primary" />
-            <p className="text-primary font-semibold">Back to Dashboard</p>
-          </Link>
-          <section className="flex gap-8">
-            <div className="w-7/12 space-y-8 flex items-center justify-center">
-              <img
-                className="rounded object-contain"
-                src="/images/rocket.png"
-                alt=""
-              />
-            </div>
-            <div className="md:w-5/12 w-full space-y-2">
-              <h1 className="text-center text-primary font-semibold">
-                Create Account
-              </h1>
-              <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={handleCreateEmployee}
-              >
-                <Form className="space-y-3">
-                  <Field
-                    className="block border-b border-primary bg-transparent px-4 py-2 focus:outline-none w-full"
-                    type="text"
-                    placeholder="Full Name"
-                    name="full_name"
-                  />
-                  <Field
-                    className="block border-b border-primary px-4 py-2 focus:outline-none w-full"
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                  />
-                  <Field
-                    className="block border-b border-primary px-4 py-2 focus:outline-none w-full"
-                    type="text"
-                    placeholder="Username"
-                    name="username"
-                  />
-                  <Field
-                    className="block border-b border-primary px-4 py-2 focus:outline-none w-full"
-                    type="text"
-                    placeholder="Password"
-                    name="password"
-                  />
-                  <Field
-                    className="block border-b border-primary px-4 py-2 focus:outline-none w-full"
-                    type="text"
-                    placeholder="Occupation"
-                    name="occupation"
-                  />
-                  <Field
-                    className="block border-b border-primary px-4 py-2 focus:outline-none w-full"
-                    type="text"
-                    placeholder="Role"
-                    name="role"
-                  />
-                  <button className="bg-primary shadow-2xl rounded text-white w-full px-4 py-3">
-                    Send
-                  </button>
-                </Form>
-              </Formik>
-            </div>
+        <div className="bg-white rounded w-full mx-auto p-4 h-[100vh] flex items-center justify-center">
+          <section>
+            <Link href="/dashboard/admin" className="flex items-center gap-2">
+              <BiArrowBack className="text-primary" />
+              <p className="text-primary font-semibold">Back to Dashboard</p>
+            </Link>
+            <section className="flex gap-8">
+              <div className="w-1/2 space-y-8 flex items-center justify-center">
+                <img
+                  className="rounded object-contain"
+                  src="/images/rocket.png"
+                  alt=""
+                />
+              </div>
+              <div className="md:w-1/2 w-full space-y-2">
+                <h1 className="text-center mb-2 text-primary font-semibold">
+                  Create User Account
+                </h1>
+                <Formik
+                  initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={handleCreateEmployee}
+                >
+                  <Form className="space-y-3">
+                    <Field
+                      className="block border rounded-3xl border-primary bg-transparent px-4 py-3 focus:outline-none w-full"
+                      type="text"
+                      placeholder="Full Name"
+                      name="full_name"
+                    />
+                    <Field
+                      className="block border rounded-3xl border-primary px-4 py-3 focus:outline-none w-full"
+                      type="text"
+                      placeholder="Email"
+                      name="email"
+                    />
+                    <Field
+                      className="block border rounded-3xl border-primary px-4 py-3 focus:outline-none w-full"
+                      type="text"
+                      placeholder="Username"
+                      name="username"
+                    />
+                    <Field
+                      className="block border rounded-3xl border-primary px-4 py-3 focus:outline-none w-full"
+                      type="text"
+                      placeholder="Occupation"
+                      name="occupation"
+                    />
+                    <Field
+                      className="block border rounded-3xl border-primary px-4 py-3 focus:outline-none w-full"
+                      type="text"
+                      placeholder="Role"
+                      name="role"
+                    />
+                    <Field
+                      className="block border rounded-3xl border-primary px-4 py-3 focus:outline-none w-full"
+                      type="text"
+                      placeholder="Password"
+                      name="password"
+                    />
+                    <button className="bg-primary shadow-2xl rounded text-white w-full px-4 py-3">
+                      Send
+                    </button>
+                  </Form>
+                </Formik>
+              </div>
+            </section>
           </section>
         </div>
       </section>
